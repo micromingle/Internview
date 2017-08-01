8a43420eaeeeea5dd8f01d54187f112b79ce0ede
micromingle <micromingle@gmail.com>
2017-08-01 20:50:07 +0800

    add strick mode

--- Inter.java
+++ Inter.java
@@ -713,6 +713,21 @@ public class Inter {
 					  canvas.restore();
 
 			 
+			 22   严格模式
+			 
+			     ThreadPolicy线程策略检测
+
+				线程策略检测的内容有
+				自定义的耗时调用 使用detectCustomSlowCalls()开启
+				磁盘读取操作 使用detectDiskReads()开启
+				磁盘写入操作 使用detectDiskWrites()开启
+				网络操作 使用detectNetwork()开启
+				VmPolicy虚拟机策略检测
+
+				Activity泄露 使用detectActivityLeaks()开启
+				未关闭的Closable对象泄露 使用detectLeakedClosableObjects()开启
+				泄露的Sqlite对象 使用detectLeakedSqlLiteObjects()开启
+				检测实例数量 使用setClassInstanceLimit()开启
 			 
 			// 20   NestedScrolling 机制
 				 
